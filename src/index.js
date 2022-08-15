437c14475d41900ea60b3c8eccfd7be74fddc8a8
GabeenKim <rlarkqls5551250@gmail.com>
2022-08-16 02:51:09 +0900

    최상단경로수정

--- src/index.js
+++ src/index.js
@@ -30,10 +30,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/api', api);
 app.get('/', (req, res) => {
   res.send('Hello');
 });
+app.use('/api', api);
 
 app.listen(port, () => {
   console.log(`서버실행 => http://localhost:${port}`);
