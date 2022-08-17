0fa00871714528a3213d7b8ddd0c3b57fa1b0217
GabeenKim <rlarkqls5551250@gmail.com>
2022-08-17 14:00:52 +0900

    trainData 수정

--- seeders/20220803232625-compSeat.js
+++ seeders/20220803232625-compSeat.js
@@ -2,63 +2,77 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const { add } = require('date-fns');
+    const { add, set } = require('date-fns');
 
     const trainType = ['KTX', '새마을호', '무궁화호'];
-    const now = new Date();
-
+    const now = new Date('2022-08-20T09:00:00Z');
+    console.log(now);
     for (let i = 0; i < 3; i++) {
       await queryInterface.bulkInsert('Trains', [
         //서울-김천구미
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 1, hours: 9 }),
-          arr_pland_time: add(now, { days: i + 1, hours: i + 10 }),
+          dep_pland_time: add(now, { hours: 0 }), //9시
+          arr_pland_time: add(now, { hours: 1 + i, minutes: 15 }),
           dep_place_name: '서울',
           arr_place_name: '김천구미',
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 1, hours: 10, minutes: 30 }),
-          arr_pland_time: add(now, { days: i + 1, hours: i + 11, minutes: 30 }),
+          dep_pland_time: add(now, { hours: 2, minutes: 25 }),
+          arr_pland_time: add(now, { hours: 3 + i, minutes: 40 }),
           dep_place_name: '서울',
           arr_place_name: '김천구미',
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 1, hours: 11, minutes: 15 }),
-          arr_pland_time: add(now, { days: i + 1, hours: i + 12, minutes: 15 }),
+          dep_pland_time: add(now, { hours: 4, minutes: 15 }),
+          arr_pland_time: add(now, { hours: 5 + i, minutes: 30 }),
           dep_place_name: '서울',
           arr_place_name: '김천구미',
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 1, hours: 12, minutes: 25 }),
-          arr_pland_time: add(now, { days: i + 1, hours: i + 13, minutes: 25 }),
+          dep_pland_time: add(now, { hours: 6, minutes: 25 }),
+          arr_pland_time: add(now, { hours: 7 + i, minutes: 40 }),
           dep_place_name: '서울',
           arr_place_name: '김천구미',
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 1, hours: 13 }),
-          arr_pland_time: add(now, { days: i + 1, hours: i + 14 }),
+          dep_pland_time: add(now, { hours: 8 }),
+          arr_pland_time: add(now, { hours: 9 + i, minutes: 15 }),
+          dep_place_name: '서울',
+          arr_place_name: '김천구미',
+        },
+        {
+          train_grade_name: trainType[i],
+          dep_pland_time: add(now, { hours: 10 }),
+          arr_pland_time: add(now, { hours: 11 + i, minutes: 15 }),
+          dep_place_name: '서울',
+          arr_place_name: '김천구미',
+        },
+        {
+          train_grade_name: trainType[i],
+          dep_pland_time: add(now, { hours: 12 }),
+          arr_pland_time: add(now, { hours: 13 + i, minutes: 15 }),
           dep_place_name: '서울',
           arr_place_name: '김천구미',
         },
         //수원-부산
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 10, hours: 9, minutes: 30 }),
-          arr_pland_time: add(now, { days: i + 10, hours: i + 12 }),
+          dep_pland_time: add(now, { days: 10, hours: 0, minutes: 30 }),
+          arr_pland_time: add(now, { days: 10, hours: i + 2 }),
           dep_place_name: '수원',
           arr_place_name: '부산',
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 10, hours: 10, minutes: 25 }),
+          dep_pland_time: add(now, { days: 10, hours: 1, minutes: 25 }),
           arr_pland_time: add(now, {
-            days: i + 10,
-            hours: i + 12,
+            days: 10,
+            hours: i + 3,
             minutes: 55,
           }),
           dep_place_name: '수원',
@@ -66,10 +80,10 @@ module.exports = {
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 10, hours: 11, minutes: 40 }),
+          dep_pland_time: add(now, { days: 10, hours: 3, minutes: 40 }),
           arr_pland_time: add(now, {
-            days: i + 10,
-            hours: i + 14,
+            days: 10,
+            hours: i + 6,
             minutes: 10,
           }),
           dep_place_name: '수원',
@@ -77,10 +91,10 @@ module.exports = {
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 10, hours: 12, minutes: 40 }),
+          dep_pland_time: add(now, { days: 10, hours: 5, minutes: 40 }),
           arr_pland_time: add(now, {
-            days: i + 10,
-            hours: i + 15,
+            days: 10,
+            hours: i + 8,
             minutes: 10,
           }),
           dep_place_name: '수원',
@@ -88,8 +102,22 @@ module.exports = {
         },
         {
           train_grade_name: trainType[i],
-          dep_pland_time: add(now, { days: i + 10, hours: 13 }),
-          arr_pland_time: add(now, { days: i + 10, hours: i + 15 }),
+          dep_pland_time: add(now, { days: 10, hours: 7 }),
+          arr_pland_time: add(now, { days: 10, hours: i + 9, minutes: 30 }),
+          dep_place_name: '수원',
+          arr_place_name: '부산',
+        },
+        {
+          train_grade_name: trainType[i],
+          dep_pland_time: add(now, { days: 10, hours: 9 }),
+          arr_pland_time: add(now, { days: 10, hours: i + 11, minutes: 30 }),
+          dep_place_name: '수원',
+          arr_place_name: '부산',
+        },
+        {
+          train_grade_name: trainType[i],
+          dep_pland_time: add(now, { days: 10, hours: 11 }),
+          arr_pland_time: add(now, { days: 10, hours: i + 13, minutes: 30 }),
           dep_place_name: '수원',
           arr_place_name: '부산',
         },
